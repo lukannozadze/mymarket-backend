dd9f60d10817c847f1c93c4338773ffd359d54c4
lukannozadze <nozadzeluka2002@gmail.com>
2024-07-24 13:06:45 +0400

    comment web chat code

--- app/app.js
+++ app/app.js
@@ -1,33 +1,33 @@
-const socket = io("ws://localhost:3000");
+// const socket = io("ws://localhost:3000");
 
-const nameContainer = document.querySelector(".name-container");
-const chatContainer = document.querySelector(".chat-container");
+// const nameContainer = document.querySelector(".name-container");
+// const chatContainer = document.querySelector(".chat-container");
 
-document.querySelector(".name-input").addEventListener("submit", (e) => {
-  e.preventDefault();
-  nameContainer.style.display = "none";
-  chatContainer.style.display = "flex";
-});
+// document.querySelector(".name-input").addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   nameContainer.style.display = "none";
+//   chatContainer.style.display = "flex";
+// });
 
-function sendMessage(e) {
-  e.preventDefault();
-  const input = document.querySelector(".message-input");
-  const message = input.value;
-  const name = document.querySelector(".username").value;
-  if (message) {
-    socket.send(message, name);
-    input.value = "";
-  }
-  input.focus();
+// function sendMessage(e) {
+//   e.preventDefault();
+//   const input = document.querySelector(".message-input");
+//   const message = input.value;
+//   const name = document.querySelector(".username").value;
+//   if (message) {
+//     socket.send(message, name);
+//     input.value = "";
+//   }
+//   input.focus();
 
-  return false;
-}
+//   return false;
+// }
 
-document.querySelector(".chat-form").addEventListener("submit", sendMessage);
+// document.querySelector(".chat-form").addEventListener("submit", sendMessage);
 
-socket.addEventListener("message", (message) => {
-  console.log(message);
-  const el = document.createElement("li");
-  el.textContent = ` ${message}`;
-  document.querySelector("ul").appendChild(el);
-});
+// socket.addEventListener("message", (message) => {
+//   console.log(message);
+//   const el = document.createElement("li");
+//   el.textContent = ` ${message}`;
+//   document.querySelector("ul").appendChild(el);
+// });
